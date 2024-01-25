e22a56c2eafce6f62657036087b0dd50f2432cd7
dannygorgon <thirdoptionisthename@googlemail.com>
2024-01-25 23:17:22 +0000

    add dotenv, add WIP routes

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    const responseBody = {message: "Hello World!"}  
+    res.status(200).send(responseBody)                      
 })
 
+app.post("/", (req, res) => {
+    const {name, age} = req.body
+    res.status(200).send({message: `Hello ${name}, you are ${age} years old!`} )
+})
+
+app.patch("/", (req, res) => {
+    const {name, age} = req.body
+    res.status(200).send({message: `Successfully patched! Hello ${name}, you are ${age} years old!`} )
+})
 module.exports = app
